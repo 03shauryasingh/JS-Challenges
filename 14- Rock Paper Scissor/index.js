bea594e7299292a46ae030e19f1328a1d5b617e8
Shaurya Singh <shawryathakur@gmail.com>
2024-08-04 06:42:01 +0530

    all exercises done

--- 14- Rock Paper Scissor/index.js
+++ 14- Rock Paper Scissor/index.js
@@ -1,3 +1,8 @@
 let hands = ["rock", "paper", "scissor"];
 
 // Create a function that returns a random item from the array
+function rps() {
+  let randomIndex = Math.floor(Math.random() * hands.length);
+  return hands[randomIndex];
+}
+console.log(rps());
--- 15- Emoji Fighter/index.js
+++ 15- Emoji Fighter/index.js
@@ -25,4 +25,11 @@ fightButton.addEventListener("click", function () {
   // Challenge:
   // When the user clicks on the "Pick Fighters" button, pick two random
   // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+
+  let randomE1 = Math.floor(Math.random() * fighters.length);
+
+  let randomE2 = Math.floor(Math.random() * fighters.length);
+
+  stageEl.textContent =
+    fighters[randomE1] + " " + "VS" + " " + fighters[randomE2];
 });
